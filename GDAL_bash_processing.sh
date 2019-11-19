7293f99011b86fc4bfe5f192615db3e34a8f2f1b
patrick_m_norman <44315268+panorm@users.noreply.github.com>
2019-11-19 16:30:29 +1000

    Update GDAL_bash_processing.sh

--- GDAL_bash_processing.sh
+++ GDAL_bash_processing.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #this script clips and polygonizes a reclassified normalized burn ratio raster
 #this line is reprojecting a shapefile and converts it to a spatialite polygon which will be used to clip a raster
-ogr2ogr -select CLIP_POLYGON_LAYER -t_srs EPSG:YOUR_CRS_EPSG -f SQLite projected_clip_polygon.sqlite CLIP_POLYGON.shp
+ogr2ogr -select CLIP_POLYGON_LAYER -t_srs EPSG:YOUR_CRS_EPSG -f SQLite projected_clip_polygon.sqlite YOUR_VEGETATION_POLYGON.shp
 #reprojecting and clipping a raster to the shapefile above 
-gdalwarp -t_srs EPSG:28356 -cutline projected_clip_polygon.sqlite -cl CLIP_POLYGON Burn_ratio.tif Burn_proj.tif
+gdalwarp -t_srs EPSG:28356 -cutline projected_clip_polygon.sqlite -cl YOUR_VEGETATION_POLYGON Burn_ratio.tif Burn_proj.tif
 #converting any raster cells less than 5 to the surrounding value.
 #this cleans a lot of the small errors from the burn ratio calculation
 gdal_sieve.py -st 5 Burn_proj.tif Burn_proj_clean.tif
